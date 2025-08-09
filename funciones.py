71eeb862475298805e2a1d19c4cdc27e083cbcfc
CrashhSus <santidavila233@gmail.com>
2025-08-08 20:16:45 -0500

    Version 8

--- funciones.py
+++ funciones.py
@@ -3,10 +3,8 @@
 def multiplicacion(a,b):
     return a * b
 
-<<<<<<< HEAD
 def division(a,b):
     if b == 0:
         return "Error: no se puede dividir por cero"
     return a / b
 >>>>>>> cristian
-=======
