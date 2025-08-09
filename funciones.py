c51d2492b659574e99eca35945d1392a88c05ed4
cristian-andres-criollo <tovarcristian431@gmail.com>
2025-08-08 19:08:13 -0500

    version dos de cristian

--- funciones.py
+++ funciones.py
@@ -1,3 +1,4 @@
+
 def multiplicacion(a,b):
     return a * b
 
@@ -5,3 +6,9 @@ def division(a,b):
     if b == 0:
         return "Error: no se puede dividir por cero"
     return a / b
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
