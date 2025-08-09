1816a348dc7dd206afc9ff4bd87b847ccd91cfda
CrashhSus <santidavila233@gmail.com>
2025-08-08 19:09:26 -0500

    Funcion y menu suma y resta de Santiago

--- funciones.py
+++ funciones.py
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-def multiplicacion(a,b):
-    return a * b
-
-def division(a,b):
-    if b == 0:
-        return "Error: no se puede dividir por cero"
-    return a / b
-=======
 def sumar(a, b):
     return a + b
 
 def restar(a, b):
-<<<<<<< HEAD
     return a - b
->>>>>>> 59f3fc742e8891305e10ba5b78311dbf55cd9730
-=======
-    return a - b
->>>>>>> Santiago
--- menu.py
+++ menu.py
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-from funciones import multiplicacion, division
-def mostrar_menu():
-    print("Seleccione una operación:")
-    print("4. Multiplicación")
-    print("5. División")
-    print("6. Salir")
-    opcion = input("Ingrese su opción: ")
-    return opcion
-def ejecutar_operacion(opcion, a, b):
-    if opcion == '4':
-        resultado = multiplicacion(a, b)
-        print(f"El resultado de la multiplicación es: {resultado}")
-    elif opcion == '5':
-        resultado = division(a, b)
-        print(f"El resultado de la división es: {resultado}")
-    elif opcion == '6':
-        print("Saliendo del programa.")
-
-if __name__ == "__main__":
-    while True:
-        opcion = mostrar_menu()
-        if opcion == '6':
-            ejecutar_operacion(opcion, 0, 0)
-            break
-        try:
-            a = float(input("Ingrese el primer número: "))
-            b = float(input("Ingrese el segundo número: "))
-            ejecutar_operacion(opcion, a, b)
-        except ValueError:
-            print("Error: Entrada no válida. Por favor, ingrese números válidos.")
-=======
-from funciones import sumar, restar
-=======
 from funciones import sumar, restar, multiplicacion, division
->>>>>>> Santiago
 
 def mostrar_menu():
     print("Calculadora Básica")
@@ -66,11 +30,4 @@ while True:
     if opcion == "1":
         resultado = sumar(num1, num2)
     elif opcion == "2":
-<<<<<<< HEAD
         resultado = restar(num1, num2)
-
-    print(f"Resultado: {resultado}\n")
->>>>>>> 59f3fc742e8891305e10ba5b78311dbf55cd9730
-=======
-        resultado = restar(num1, num2)
->>>>>>> Santiago
